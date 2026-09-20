2ba62ac0f175175307159514429bd4bab54279ed
shaheen2794511-prog <shaheen2794511@gmail.com>
2026-09-20 13:25:09 +0530

    Update FinishedState.java

--- src/state/FinishedState.java
+++ src/state/FinishedState.java
@@ -1,28 +1,24 @@
-package state;
-
-public class FinishedState implements RaceState {
+public class FinishedState
+        implements RaceState {
 
     @Override
     public void start(Race race) {
+
         throw new IllegalStateException(
-                "Race has already finished");
+                "Race already finished");
     }
 
     @Override
-    public void runTick(Race race) {
+    public void tick(Race race) {
+
         throw new IllegalStateException(
-                "Race has already finished");
+                "Race already finished");
     }
 
     @Override
     public void finish(Race race) {
-        throw new IllegalStateException(
-                "Race has already finished");
-    }
 
-    @Override
-    public void cancel(Race race) {
-        throw new IllegalStateException(
-                "Finished race cannot be cancelled");
+        System.out.println(
+                "Race has already finished");
     }
 }
