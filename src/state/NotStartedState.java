06587ed4ef1a717e1222ea64738c08cb9be56374
shaheen2794511-prog <shaheen2794511@gmail.com>
2026-09-20 13:23:52 +0530

    Update NotStartedState.java

--- src/state/NotStartedState.java
+++ src/state/NotStartedState.java
@@ -3,22 +3,24 @@ public class NotStartedState
 
     @Override
     public void start(Race race) {
+
         race.setState(new RunningState());
+
+        System.out.println(
+                "Starting race...");
     }
 
     @Override
     public void tick(Race race) {
+
         throw new IllegalStateException(
                 "Race has not started");
     }
 
     @Override
     public void finish(Race race) {
-        throw new IllegalStateException();
-    }
 
-    @Override
-    public void cancel(Race race) {
-        race.setState(new CancelledState());
+        throw new IllegalStateException(
+                "Race has not started");
     }
 }
